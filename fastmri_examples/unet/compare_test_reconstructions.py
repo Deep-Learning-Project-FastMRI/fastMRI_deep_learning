69809b7c9630d5e958dd6e01e306fc5dccc54935
Sahithya Pasagada <spasagada3@gatech.edu>
2025-03-26 18:29:40 -0400

    add get method

--- fastmri_examples/unet/compare_test_reconstructions.py
+++ fastmri_examples/unet/compare_test_reconstructions.py
@@ -1,6 +1,7 @@
 import os
 import pickle
-
+import h5py
+import numpy as np
 
 # for every reconstruction file in benchmark and manual
 
@@ -10,11 +11,26 @@ def compare(ground_truth_folder, reconstruction_test_folder):
         # get the reconstructed image
         # get the ROI for both
         # calculate SSIM/PSNR/MSE/etc
-    pass
+    
+    ground_truth_files = sorted(os.listdir(ground_truth_folder))
+    for gt_file in ground_truth_files:
+        print(get_gt(gt_file))
 
+def get_gt(fname):
+        print("FNAME HERE IS: ")
+        print(fname)
+        with h5py.File(fname, "r") as hf:
+            kspace = hf["kspace"][dataslice]
 
+            mask = np.asarray(hf["mask"]) if "mask" in hf else None
 
-reconstruction_test_folder = "unet_logging/manual/reconstructions/reconstructions/reconstruction_test"
+            target = hf["reconstruction_rss"][dataslice] 
 
-ground_truth_folder = ".../.../brain_data/multicoil_test/"
+        return target
+
+# reconstruction_test_folder = "unet_logging/manual/reconstructions/reconstructions/reconstruction_test"
+
+reconstruction_test_folder = None
+
+ground_truth_folder = "../../brain_data/multicoil_test/"
 compare(ground_truth_folder, reconstruction_test_folder)
