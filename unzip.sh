042c2b84462dfb66518bec9517354be645bc1a49
Sahithya Pasagada <spasagada3@gatech.edu>
2025-03-27 16:16:39 -0400

    fix nmse calc

---
+++ unzip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+tar -xf subsample_val_4.tar.gz
+echo "DONE_4_val"
+tar -xf subsample_val_5.tar.gz
+echo "DONE_5_val"
+tar -xf subsample_val_6.tar.gz
+echo "DONE_6_val"
+tar -xf subsample_val_7.tar.gz
+echo "DONE_7_val"
